234121957916350d07e84ed6b75ae86d58b18b25
KNineLat <knine@canine.lat>
2026-06-12 12:39:13 +0200

    Add mini exam computing the average of nightly howls

---
+++ basics/04-mini-exam-howl-average.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int first_day_howls;
+    int second_day_howls;
+    int final_day_howls;
+    int total_howls;
+    float average_howls_count;
+
+    printf("How many times did you hear wolves howl the night before last: ");
+    scanf("%d", &first_day_howls);
+    printf("How many times did you hear wolves howl last night: ");
+    scanf("%d", &second_day_howls);
+    printf("How many times did you hear wolves howl tonight: ");
+    scanf("%d", &final_day_howls);
+
+    total_howls = first_day_howls + second_day_howls + final_day_howls;
+    average_howls_count = total_howls / 3.0f;
+
+    printf("In total you heard wolves howl %d times. "
+           "You heard an average of %.0f howls a night (exact value: %.2f).\n",
+           total_howls, average_howls_count, average_howls_count);
+
+    return 0;
+}
