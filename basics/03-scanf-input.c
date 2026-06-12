a5f873ab231963ef1ecc967f67ee94ba3fd65bf4
KNineLat <knine@canine.lat>
2026-06-12 12:39:09 +0200

    Add lesson on reading user input with scanf

---
+++ basics/03-scanf-input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+    int wolves;
+
+    printf("How many wolves do you see daily: ");
+    scanf("%d", &wolves);
+
+    printf("AWwoooo %d times. Tomorrow you will see %d wolves.\n", wolves, wolves + 42);
+    return 0;
+}
